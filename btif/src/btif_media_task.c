f249a7bd9713edb9bdb87408813dca10ff5e43fb
eagle1967 <theeagle1967@gmail.com>
2013-06-13 13:26:54 -0700

    more bluetooth fixes

--- btif/src/btif_media_task.c
+++ btif/src/btif_media_task.c
@@ -1,6 +1,7 @@
 /******************************************************************************
  *
  *  Copyright (C) 2009-2012 Broadcom Corporation
+ *  Copyright (c) 2013, Linux Foundation. All rights reserved.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -153,7 +154,7 @@ enum {
 #define BTIF_MEDIA_BITRATE_STEP 5
 #endif
 
-/* High quality quality setting @ 44.1 khz */
+/* High quality setting @ 44.1 khz */
 #define DEFAULT_SBC_BITRATE 328
 
 #ifndef A2DP_MEDIA_TASK_STACK_SIZE
@@ -764,8 +765,12 @@ void btif_a2dp_setup_codec(void)
 
     GKI_disable();
 
-    /* for now hardcode 48 khz 16 bit stereo */
+    /* for now hardcode 44/48 khz 16 bit stereo */
+#ifdef SAMPLE_RATE_48K
     media_feeding.cfg.pcm.sampling_freq = 48000;
+#else
+    media_feeding.cfg.pcm.sampling_freq = 44100;
+#endif
     media_feeding.cfg.pcm.bit_per_sample = 16;
     media_feeding.cfg.pcm.num_channel = 2;
     media_feeding.format = BTIF_AV_CODEC_PCM;
